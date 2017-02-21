6dc267c6b246f4cca0683c8b8065df2388744e2c
logonrm <logonrm@fiap.com.br>
2017-02-20 22:15:53 -0300

    segundo commit

--- app/app.component.ts
+++ app/app.component.ts
@@ -3,10 +3,17 @@ import { Component } from '@angular/core';
 import { PrimeiroExemplo } from './exemplos/primeiro.exemplo.component';
 import { CursosComponent } from './cursos/cursos.component';
 import { DataBindingComponent } from './exemplos/data-binding.component';
+import { EventosComponent } from './exemplos/eventos.component'
 
 @Component({
     selector: 'my-app',
-    template: ' <h1>Angular 2 turma xevere</h1>  <p>Hello World!</p><primeiro-exemplo></primeiro-exemplo><cursos-lista></cursos-lista><exemplo-data-binding></exemplo-data-binding>',
-     directives : [PrimeiroExemplo, CursosComponent, DataBindingComponent]
+    template: ` <h1>Angular 2 turma xevere</h1>  
+    <p>Hello World!</p>
+    <primeiro-exemplo></primeiro-exemplo>
+    <cursos-lista></cursos-lista>
+    <exemplo-data-binding></exemplo-data-binding>
+    <eventos-binding></eventos-binding>
+    `,
+     directives : [PrimeiroExemplo, CursosComponent, DataBindingComponent, EventosComponent]
 })
 export class AppComponent { }
